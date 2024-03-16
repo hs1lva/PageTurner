0d97898907c110016ff0568670bf609b89d1dafe
Pedro Ricardo <pedroricardo10@gmail.com>
2024-03-16 16:26:03 +0000

    Pagina contexto com tabelas

--- backend/Data/PageTurnerContext.cs
+++ backend/Data/PageTurnerContext.cs
@@ -1,4 +1,5 @@
 
+using backend.Models;
 using Microsoft.EntityFrameworkCore;
 
 public class PageTurnerContext:DbContext{
@@ -8,6 +9,22 @@ public class PageTurnerContext:DbContext{
     }
 
     //escrever aqui as tabelas
+    public DbSet<AutorLivro> AutorLivro { get; set; }
+    public DbSet<AvaliacaoLivro> AvaliacaoLivro { get; set; }
+    public DbSet<Cidade> Cidade { get; set; }
+    public DbSet<ComentarioLivro> CommentLivro { get; set; }
+    public DbSet<ConteudoOfensivo> ConteudoOfensivo { get; set; }
+    public DbSet<EstadoComentario> EstadoComentario { get; set; }
+    public DbSet<EstadoConta> EstadoConta { get; set; }
+    public DbSet<EstadoTroca> EstadoTroca { get; set; }
+    public DbSet<Estante> Estante { get; set; }
+    public DbSet<GeneroLivro> GeneroLivro { get; set; }
+    public DbSet<Livro> Livro { get; set; }
+    public DbSet<Pais> Pais { get; set; }
+    public DbSet<TipoEstante> TipoEstante { get; set; }
+    public DbSet<TipoUtilizador> TipoUtilizador { get; set; }
+    public DbSet<Troca> Troca { get; set; }
+    public DbSet<Utilizador> Utilizador { get; set; }
 
     
 }
