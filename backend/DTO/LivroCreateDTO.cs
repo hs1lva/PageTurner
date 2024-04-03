e46c7dfa87cb01e20b1caad12b0aabcf78ca0ee1
Hugo Poças <hugopocas@MacBook-Pro-de-Hugo.local>
2024-04-03 11:55:57 +0100

    Update Livro model for idioma as string, add MediaAvaliacao method; update DTOs and Controller; add migration

---
+++ backend/DTO/LivroCreateDTO.cs
@@ -0,0 +1,10 @@
+namespace backend.Models;
+
+public class LivroCreateDTO
+{
+    public string TituloLivro { get; set; }
+    public int AnoPrimeiraPublicacao { get; set; }
+    public string IdiomaOriginalLivro { get; set; }
+    public int AutorLivroId { get; set; } 
+    public int GeneroLivroId { get; set; } 
+}
---
+++ backend/DTO/LivroDTO.cs
@@ -0,0 +1,14 @@
+namespace backend.Models;
+
+public class LivroDTO
+{
+    public int LivroId { get; set; }
+    public string TituloLivro { get; set; }
+    public int AnoPrimeiraPublicacao { get; set; }
+    public string IdiomaOriginalLivro { get; set; }
+    public AutorLivro AutorLivro { get; set; }
+    public GeneroLivro GeneroLivro { get; set; }
+    public ICollection<ComentarioLivro> Comentarios { get; set; }
+    public ICollection<AvaliacaoLivro> Avaliacoes { get; set; }
+    public double? MediaAvaliacao { get; set; }
+}
