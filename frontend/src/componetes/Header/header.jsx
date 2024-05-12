d856ff6570f00b75d96fd2cc36b894d195367575
Pedro Ricardo <pedroricardo10@gmail.com>
2024-05-12 16:05:16 +0100

    Pesquisa a funcionar, ajuste no SobreNos.js

--- frontend/src/componetes/Header/header.jsx
+++ frontend/src/componetes/Header/header.jsx
@@ -4,11 +4,14 @@ import imagem from "../../imagens/banner.jpg";
 import useAuthStore from "../../services/authService";
 import ApiService from "../../services/ApiService";
 import { url_server } from "../../contexto/url_servidor";
+import { useState } from "react";
+import LoadingModal from "../Loading/loading";
 
 export default function Navbar() {
   const { isAuthenticated, logout } = useAuthStore();
   const navigate = useNavigate();
   const apiService = new ApiService(url_server());
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleLogout = () => {
     logout();
@@ -17,26 +20,37 @@ export default function Navbar() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    apiService.get("/api/Livro/PesquisarLivro/OpenLibrary/Titulo", e.target.search.value)
-        .then((data) => {
-            console.log(data.docs);
-            navigate("/pesquisa", { state: { livros: data.docs } });
-        })
-        .catch((error) => console.error(error));
+    setIsLoading(true);
+    apiService
+      .get(
+        "/api/Livro/PesquisarLivro/OpenLibrary/Titulo",
+        e.target.search.value
+      )
+      .then((data) => {
+        navigate("/pesquisa", { state: { livros: data.docs } });
+      })
+      .catch((error) => console.error(error));
+    setIsLoading(false);
+  };
+
+  if (isLoading) {
+    return <LoadingModal showModal={isLoading} />;
   }
 
   return (
     <div className="nav-container">
       <nav className="nav-bar">
         <img src={imagem} width="100px" alt="" className="imagem" />
-        <form action="submit" onSubmit={handleSubmit}>
-          <input
-            className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-            placeholder="Pesquise livros, autores, leitores e muito mais..."
-            type="text"
-            name="search"
-          />
-        </form>
+        <div className="w-full">
+          <form className="w-full" action="submit" onSubmit={handleSubmit}>
+            <input
+              className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+              placeholder="Pesquise livros, autores, leitores e muito mais..."
+              type="text"
+              name="search"
+            />
+          </form>
+        </div>
         <ul className="nav-items">
           <li>
             <Link to={`/SobreNos`} className="nav-item1">
--- frontend/src/paginas/Pesquisa/titulo.js
+++ frontend/src/paginas/Pesquisa/titulo.js
@@ -1,12 +1,21 @@
-export default function Titulo( livros ) {
+import { useLocation } from "react-router-dom";
+import "./titulo.css";
 
-    console.log(livros.docs);
-
+export default function Titulo() {
+  const location = useLocation();
+  const livros = location.state.livros;
+console.log(livros)
   return (
     <div className="pesquisa-titulo">
       <div className="cards">
 
-      <h2>Resultados</h2>
+        {livros.map((livro) => (
+          <div className="card" key={livro.key}>
+            <img src={livro.capas[1]} alt={livro.title} /> {/* // Array 0 é a capa pequena, 1 é a média e 2 é a grande */}
+            <h3>{livro.title}</h3>
+            <p>{livro.author_name}</p>
+          </div>
+        ))}
         
       </div>
     </div>
