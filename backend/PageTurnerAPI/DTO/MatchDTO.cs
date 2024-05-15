ccff6fe931a344b0760c9ef62574e084240a8f95
Pedro Ricardo <pedroricardo10@gmail.com>
2024-05-15 20:26:24 +0100

    acho que está

--- backend/PageTurnerAPI/DTO/MatchDTO.cs
+++ backend/PageTurnerAPI/DTO/MatchDTO.cs
@@ -4,6 +4,6 @@ namespace backend.Models;
 public class MatchDTO
 {
     public List<Estante> ListaEstantesComLivroQueEuQuero { get; set; }
-    public List<Utilizador> ListUsersTemLivroQueEuQuero { get; set; }
+    public List<Utilizador> ListUsersTemLivroQueEuQuero { get; set; } 
     public List<Estante> ListEstantesDosUsersQuerLivroETemLivrosQueMeInteressam { get; set; }
 }
